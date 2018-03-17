a9bea9d6f6b3f0ba02b143a47d02a9d81478c551
andreyK9 <18041988@mail.ru>
2018-03-17 13:10:59 +0300

    Fix

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+'use strict';
 
 scrollSection('.sec_1', '.sec_2', 'down');
 scrollSection('.sec_2', '.sec_3', 'down');
@@ -5,31 +6,31 @@ scrollSection('.sec_2', '.sec_1', 'up');
 scrollSection('.sec_3', '.sec_2', 'up');
 
 // scrollDown link -----------------------------
-var scrollDown = document.querySelector('#scrollDown');
+const scrollDown = document.querySelector('#scrollDown');
 
 scrollDown.addEventListener('click', function (event) {
 
     event.preventDefault();
 
-    var sec_2 = document.querySelector('.sec_2');
+    const sec_2 = document.querySelector('.sec_2');
 
     smoothScroll(sec_2);
 });
 
 // toggle scroll default -----------------------------
-var year = document.querySelector('#year');
+const year = document.querySelector('#year');
 
 year.style.background = '-webkit-linear-gradient(left ,#d1eaff 1%,rgba(209, 234, 255,.2) 1%, rgba(209, 234, 255,.2) 97%, transparent 97% )';
 
 // right mnu -----------------------------------
 
-var fp_nav = document.querySelectorAll('#fp-nav a');
-var sec_1 = document.querySelector('.sec_1');
-var sec_2 = document.querySelector('.sec_2');
-var sec_3 = document.querySelector('.sec_3');
+const fp_nav = document.querySelectorAll('#fp-nav a');
+const sec_1 = document.querySelector('.sec_1');
+const sec_2 = document.querySelector('.sec_2');
+const sec_3 = document.querySelector('.sec_3');
 
-for (var i = 0; i < fp_nav.length; i++) {
-    
+for (let i = 0; i < fp_nav.length; i++) {
+
         if (i === 0) {
             fp_nav[0].addEventListener('click', function (event) {
 
@@ -37,11 +38,11 @@ for (var i = 0; i < fp_nav.length; i++) {
 
                 smoothScroll(sec_1);
             });
-            
+
         }
         else if(i === 1) {
             fp_nav[1].addEventListener('click', function (event) {
-                
+
                 event.preventDefault();
 
                 smoothScroll(sec_2);
@@ -49,7 +50,7 @@ for (var i = 0; i < fp_nav.length; i++) {
         }
         else {
             fp_nav[2].addEventListener('click', function (event) {
-                
+
                 event.preventDefault();
 
                 smoothScroll(sec_3);
@@ -58,7 +59,7 @@ for (var i = 0; i < fp_nav.length; i++) {
 }
 
 window.onscroll = function() {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
 
     if(!fp_nav[0].classList.contains('active') && scrolled < 700 )
         toggleActiveClick(0);
@@ -71,50 +72,50 @@ window.onscroll = function() {
         scrollDown.style.opacity = '0';
     else
         scrollDown.style.opacity = '1';
-}
+};
 
 function toggleActiveClick(link_numb) {
-    var active = document.querySelector('#fp-nav a.active');
+    let active = document.body.querySelector('.active');
     active.classList.remove('active');
     fp_nav[link_numb].classList.add('active');
 }
 
 
 function fun1() {
-    var rng=document.getElementById('year');
-    var vol = +rng.value;
+    const rng=document.getElementById('year');
+    let vol = +rng.value;
 
     if(vol < 60)
         rng.style.background = '-webkit-linear-gradient(left ,#d1eaff '+(vol + 1)+'%,rgba(209, 234, 255,.2) '+(vol + 1)+'%, rgba(209, 234, 255,.2) 97%, transparent 97% )';
     else if(vol < 96)
         rng.style.background = '-webkit-linear-gradient(left ,#d1eaff '+ (vol - 2 ) +'%,rgba(209, 234, 255,.2) '+ (vol - 2 ) +'%, rgba(209, 234, 255,.2) 97%, transparent 97% )';
-    else    
+    else
         rng.style.background = '-webkit-linear-gradient(left ,#d1eaff 94%,rgba(209, 234, 255,.2) 94%, rgba(209, 234, 255,.2) 97%, transparent 97% )';
 
     // scroll ----------------------------------------------------------
 
-    var sec_3 = document.querySelectorAll('.sec_3 .slide');
+    const sec_3 = document.querySelectorAll('.sec_3 .slide');
     if( vol <= 25)
-        for(var i = 0; i < sec_3.length; i++)
+        for(let i = 0; i < sec_3.length; i++)
             sec_3[i].style.transform = 'translateX(0)';
     else if(vol > 25 && vol <= 75)
-        for(var i = 0; i < sec_3.length; i++)
+        for(let i = 0; i < sec_3.length; i++)
             sec_3[i].style.transform = 'translateX(-100vw)';
-    else 
-        for(var i = 0; i < sec_3.length; i++)
+    else
+        for(let i = 0; i < sec_3.length; i++)
             sec_3[i].style.transform = 'translateX(-200vw)';
 }
 
 
 function scrollSection(sectionBegin, sectionFinish, direction) {
 
-    var scrBtn = document.querySelector(sectionBegin);
-    var scrFnsh = document.querySelector(sectionFinish);
+    const scrBtn = document.querySelector(sectionBegin);
+    const scrFnsh = document.querySelector(sectionFinish);
 
-    var touchstartX = 0;
-    var touchstartY = 0;
-    var touchendX = 0;
-    var touchendY = 0;
+    let touchstartX = 0;
+    let touchstartY = 0;
+    let touchendX = 0;
+    let touchendY = 0;
 
     scrBtn.addEventListener('touchstart', function(event) {
         touchstartY =  event.touches["0"].clientY;
@@ -124,7 +125,7 @@ function scrollSection(sectionBegin, sectionFinish, direction) {
     scrBtn.addEventListener('touchend', function(event) {
         touchendY = event.changedTouches["0"].clientY;
         handleGesure(touchstartX, touchstartY, touchendX, touchendY, direction, scrFnsh);
-    }, false); 
+    }, false);
 
 }
 
@@ -152,35 +153,35 @@ function currentYPosition() {
 }
 
 function elmYPosition(eID) {
-    var elm = eID;
-    var y = elm.offsetTop;
-    var node = elm;
-    while (node.offsetParent && node.offsetParent != document.body) {
+    let elm = eID;
+    let y = elm.offsetTop;
+    let node = elm;
+    while (node.offsetParent && node.offsetParent !== document.body) {
         node = node.offsetParent;
         y += node.offsetTop;
     } return y;
 }
 
 function smoothScroll(eID) {
-    var startY = currentYPosition();
-    var stopY = elmYPosition(eID);
-    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    let startY = currentYPosition();
+    let stopY = elmYPosition(eID);
+    let distance = stopY > startY ? stopY - startY : startY - stopY;
     if (distance < 100) {
         scrollTo(0, stopY); return;
     }
-    var speed = Math.round(distance / 1300);
+    let speed = Math.round(distance / 1300);
     if (speed >= 20) speed = 20;
-    var step = Math.round(distance / 1000);
-    var leapY = stopY > startY ? startY + step : startY - step;
-    var timer = 0;
+    let step = Math.round(distance / 1000);
+    let leapY = stopY > startY ? startY + step : startY - step;
+    let timer = 0;
 
     if (stopY > startY) {
-        for ( var i=startY; i<stopY; i+=step ) {
+        for ( let i=startY; i<stopY; i+=step ) {
             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
             leapY += step; if (leapY > stopY) leapY = stopY; timer++;
         } return;
     }
-    for ( var i=startY; i>stopY; i-=step ) {
+    for ( let i=startY; i>stopY; i-=step ) {
         setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
         leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
     }
